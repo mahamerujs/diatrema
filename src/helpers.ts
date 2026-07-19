15b885f7cbbf6db5449d9134e01c65b7c4532824
Bintan <siregarbintan@gmail.com>
2026-07-19 13:58:20 +0800

    Remove unused functions

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,29 +1,6 @@
 import { access, constants } from "node:fs/promises";
-import type { HTTPMethod, ProtectedRoute } from "./types";
 import { resolve, sep } from "node:path";
 import { pathToFileURL } from "node:url";
-
-const matchRoutePattern = (currentPath: string, routePattern: string): boolean => {
-    const regexPattern = routePattern
-        .replace(/\//g, '\\/')
-        .replace(/:[^/]+/g, '[^/]+');
-
-    const regex = new RegExp(`^${regexPattern}$`);
-
-    return regex.test(currentPath);
-};
-
-export const validateProtectedRoute = (protectedRoutes: ProtectedRoute, method: string, path: string): boolean => {
-    return protectedRoutes.some(route => {
-        if (typeof route === 'string')
-            return matchRoutePattern(path, route);
-
-        const isPathMatch = matchRoutePattern(path, route.path);
-        const isMethodMatch = route.methods.includes(method as HTTPMethod);
-
-        return isPathMatch && isMethodMatch;
-    });
-};
 
 export const exists = async (target: string): Promise<boolean> => {
     try {
